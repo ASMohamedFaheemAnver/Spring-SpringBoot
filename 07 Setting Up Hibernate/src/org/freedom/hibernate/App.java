e5e7d9ff63ecba85a56f62645fa23691b20e9a37
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-10-02 10:06:03 +0530

    Spring with hibernate minimal setup,

--- 07 Setting Up Hibernate/src/org/freedom/hibernate/App.java
+++ 07 Setting Up Hibernate/src/org/freedom/hibernate/App.java
@@ -9,7 +9,7 @@ import org.hibernate.cfg.Configuration;
 
 public class App {
 	public static void main(String[] args) {
-		listingOutDataUsingORM();
+		writeUsingORM();
 	}
 
 	// Will used to write database
---
+++ 08 Spring with Hibernate/src/org/freedom/app/controller/AppController.java
@@ -0,0 +1,13 @@
+package org.freedom.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AppController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String homepage() {
+		return "homepage";
+	}
+}
