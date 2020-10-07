4875a287bb2f7a28e1185a8925d2de1563ce835a
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-10-07 15:59:07 +0530

    Redir,

--- 08 Spring with Hibernate - Config/src/org/freedom/app/controller/AppController.java
+++ 08 Spring with Hibernate - Config/src/org/freedom/app/controller/AppController.java
@@ -1,26 +1,36 @@
 package org.freedom.app.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import org.freedom.app.dao.ProductDAO;
-import org.freedom.app.entity.Product;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
-	
-	@Autowired
-	private ProductDAO products;
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String welcome(Model model) {
-		List<Product> productList = products.getProducts();
-		model.addAttribute("products", productList);
-		return "products";
+	public String welcome() {
+		return "hello";
 	}
 
+	// @RequestMapping("display-name")
+	// @RequestMapping(value="/display-name")
+	// Only get method can be executed
+	@GetMapping("display-name")
+	public ModelAndView displayName(@RequestParam("first_name") String firstName) {
+		ModelAndView modelAndView = new ModelAndView("display-name");
+		Date date = new Date();
+		List<String> names = new ArrayList<>();
+		names.add("FreeDoM");
+		names.add("Drago");
+		modelAndView.addObject("date", date);
+		modelAndView.addObject("first_name", firstName);
+		modelAndView.addObject("team", names);
+		return modelAndView;
+	}
 }
