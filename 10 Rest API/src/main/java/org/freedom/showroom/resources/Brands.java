459637dd8ef523392ee16df607d33f724b1062f0
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-10-12 12:43:37 +0530

    Jersey request mapping,

---
+++ 10 Rest API/src/main/java/org/freedom/showroom/resources/Brands.java
@@ -0,0 +1,16 @@
+package org.freedom.showroom.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/showroom")
+public class Brands {
+	@GET
+	@Path("/brands")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getBrands() {
+		return "list of brands";
+	}
+}
