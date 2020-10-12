fdf8ccb115d18f41753a91941c4fb4bb9742c253
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-10-12 13:13:10 +0530

    HTTP Request methods and Request params,

--- 10 Rest API/src/main/java/org/freedom/showroom/resources/Brands.java
+++ 10 Rest API/src/main/java/org/freedom/showroom/resources/Brands.java
@@ -1,16 +1,41 @@
 package org.freedom.showroom.resources;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/showroom")
 public class Brands {
 	@GET
-	@Path("/brands")
+	@Path("/get-brands")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getBrands() {
-		return "list of brands";
+		return "list of brands!";
+	}
+	
+	@POST
+	@Path("/set-brand")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String setBrand() {
+		return "Brand was set!";
+	}
+	
+	@PUT
+	@Path("/put-brand/{brandId}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String putBrand(@PathParam("brandId") int brandId) {
+		return "Brand with id " + brandId + "  was updated!";
+	}
+	
+	@DELETE
+	@Path("/delete-brand/{brandId}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteBrand(@PathParam("brandId") int brandId) {
+		return "Brand with id " + brandId + " was deleted!";
 	}
 }
