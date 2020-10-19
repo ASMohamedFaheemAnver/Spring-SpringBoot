d5fd0ffc91f186711816af4a18a8ba505fe3637e
freedom <jstrfaheem065@gmail.com>
2020-10-19 17:53:19 +0530

    Springboot Big GO!,

---
+++ 12 Spring Boot Starter/src/main/java/org/freedom/springboot/BlogAPIApp.java
@@ -0,0 +1,12 @@
+package org.freedom.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAPIApp {
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAPIApp.class, args);
+	}
+
+}
